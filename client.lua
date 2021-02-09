39dcf4b81e6fc066ebf005135b3d54489d02e79c
MagnusTheDev <73444626+MagnusTheDev@users.noreply.github.com>
2021-02-09 17:48:00 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,12 +1,6 @@
-Citizen.CreateThread(function()
-  Citizen.Wait(500000)
-  print("----------------------------- \nMagnus_antisay \nAlle rettigheder forbeholdes \nIngen videre salg er tilladt \n-----------------------------")
-end)
-
-
 Citizen.CreateThread(function() TriggerEvent('chat:addSuggestion', '/say') end)
 RegisterCommand("say", function(source, args, rawCommandString)
   local player = GetPlayerPed(-1)
   local coord = GetEntityCoords(player)
-    TriggerEvent('chatMessage', "Denne kommand er deaktiveret.")
+    TriggerEvent('chatMessage', "This command is deactivated.")
 end, false)
